de5da874be214077b7e6004df21893bc98d524bb
matbarcelo <42380434+matbarcelo@users.noreply.github.com>
2018-08-28 20:24:11 -0300

    Delete scrip.R

--- scrip.R
+++
@@ -1,4 +0,0 @@
-library(tidyverse)
-library(dplyr)
-parks <- read_csv("parks.csv")
-species <- read_csv("species.csv")  %>% filter(Category == "Bird", Order == "Strigiformes") %>% select("Park Name", "Conservation Status", "Scientific Name")

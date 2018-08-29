be4bae7e05845846689dbd8d503ddc458c43a166
matbarcelo <chaskavox@gmail.com>
2018-08-28 21:11:13 -0300

    upload

--- scrip.R
+++ scrip.R
@@ -2,3 +2,5 @@ library(tidyverse)
 library(dplyr)
 parks <- read_csv("parks.csv")
 species <- read_csv("species.csv")  %>% filter(Category == "Bird", Order == "Strigiformes") %>% select("Park Name", "Conservation Status", "Scientific Name")
+
+colnames(parks)[colnames(parks)=="Park Name"] <- "ParkName"
